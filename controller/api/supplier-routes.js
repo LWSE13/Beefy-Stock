6e66d047c49b67a1e25c6f629b8317fb0b84b092
Your Name <you@example.com>
2024-04-25 18:38:06 +0100

    Added in routes to 'supplier-routes.js'

--- controller/api/supplier-routes.js
+++ controller/api/supplier-routes.js
@@ -1,3 +1,76 @@
 //supplier routes
 const router = require('express').Router();
 const { Supplier, Product } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const supplierData = await Supplier.findAll({
+      include: {
+        model: Product,
+        attributes: ['product_name']
+      }
+    });
+    res.status(200).json(supplierData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+      const supplierData = await Supplier.findByPk(req.params.id, {
+        include: {
+          model: Product,
+          attributes: ['product_name']
+        }
+      });
+      res.status(200).json(supplierData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+  router.post('/', async (req, res) => {
+    try {
+      const supplierData = await Supplier.create({
+        supplier_name: req.body.supplier_name
+      });
+      res.status(200).json({ message: 'Data successfully written', supplierData, });
+    }catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+  router.put('/:id', async (req, res) => {
+    try {
+      const supplierData = await Supplier.update(req.body, {
+        where: {
+          id: req.params.id
+        }
+      });
+      if (!supplierData[0]) {
+        res.status(404).json({ message: 'No tag found with this id!' });
+        return;
+      }
+      res.status(200).json({ message: 'Data successfully updated', supplierData, });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
+
+  router.delete('/:id', async (req, res) => {
+    try {
+      const supplierData = await Supplier.destroy({
+        where: {
+          id: req.params.id
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
