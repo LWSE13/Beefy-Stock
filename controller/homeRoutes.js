32ee915db48699260d36e434e4b9d421b913b486
Ali <iodk123@outlook.com>
2024-05-02 21:35:17 +0100

    completed dashboard page

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
+const { Product } = require('../models');
+const { Op } = require('sequelize');
 
 router.get('/', (req, res) => {
-  if (req.session.loggedIn) {
-    const name = req.session.name || ''; // Get the name from session or set it to an empty string
-    res.render('dashboard', { loggedIn: true, name }); // Pass the loggedIn and name variables to the template
-  } else {
-    res.render('homepage'); // Render the homepage if the user is not logged in
-  }
+  const loggedIn = req.session.loggedIn || false; // Check if user is logged in
+  const name = req.session.name || ''; // Get the name from session or set it to an empty string
+  
+  res.render('homepage', { loggedIn, name }); // Pass the loggedIn and name variables to the template
 });
+
   router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
       res.redirect('/');
@@ -18,5 +19,38 @@ router.get('/', (req, res) => {
     res.render('login');
   });
 
+  router.get('/search', withAuth, async (req, res) => {
+    const searchQuery = req.query.productSearch;
+    const products = await Product.findAll({
+      where: {
+        product_name: {
+          [Op.iLike]: '%' + searchQuery + '%'
+        }
+      }
+    });
+    res.render('search', { 
+      products: products.map(product => product.get({ plain: true })),
+      loggedIn: req.session.loggedIn,
+      name: req.session.name
+    });
+  });
+  
+  router.get('/products/:id', withAuth, async (req, res) => {
+    try {
+      const product = await Product.findByPk(req.params.id);
+      if (product) {
+        res.render('products', { 
+          product: product.get({ plain: true }),
+          loggedIn: req.session.loggedIn,
+          name: req.session.name
+        });
+      } else {
+        res.status(404).send('Product not found');
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Something went wrong!');
+    }
+  });
 
   module.exports = router;
